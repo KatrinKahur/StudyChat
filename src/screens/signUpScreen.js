19a0c6773ab63c4a4a1d08779ab0a4fd7dcd4c37
Abdullah Trabulsiah <abdtra@kth.se>
2022-05-04 16:05:30 +0200

    Not done, but to be tested

--- src/screens/signUpScreen.js
+++ src/screens/signUpScreen.js
@@ -1,7 +1,61 @@
-import {Text, View} from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { TextInput } from 'react-native';
+import { borderBottomColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
-export default function SignUpScreen(){
-    return(
-        <View><Text>Sign-up screen</Text></View>
-    )
+export default function SignUp() {
+  return (
+    <View style={styles.SignUp}>
+
+        <Text style={styles.header}>Create a new account</Text>
+        <TextInput style={styles.textinput} placeholder="Full name" underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placeholder="Email" underlineColorAndroid={'transparent'}/>
+        <TextInput style={styles.textinput} placeholder="Password" secureTextEntry={true} underlineColorAndroid={'transparent'}/>
+
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.btntext}>Sign up</Text>
+        </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  
+  
+  SignUp: {
+    alignSelf: 'stretch',
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#f8f8f8',
+    paddingLeft: 60,
+    paddingRight: 60,
+  },
+  header: {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: '#000000',
+      paddingBottom: 10,
+      marginBottom: 40,
+      borderBottomColor: '#199187',
+      borderBottomWidth: 1,
+  },
+  textinput: {
+      alignSelf: 'stretch',
+      height: 40,
+      marginBottom: 30,
+      color: '#fff',
+      borderBottomColor: '#f8f8f8',
+      borderBottomWidth: 1,
+  },
+  button: {
+      alignSelf: 'stretch',
+      alignItems: 'center',
+      padding: 20,
+     backgroundColor: '#1e90ff',
+     marginTop: 30,
+  },
+  btntext: {
+      color: '#fff',
+      fontWeight: 'bold',
+  }
+});
