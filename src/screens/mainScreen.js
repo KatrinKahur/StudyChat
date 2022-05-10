3f73fd23f8c7191b54685e9692bc4dc164739c50
Katrin Kahur <kahur@kth.se>
2022-05-10 19:08:18 +0200

    Added navigation buttons to MainScreen.

--- src/screens/mainScreen.js
+++ src/screens/mainScreen.js
@@ -1,25 +1,16 @@
 import {View, Button} from "react-native";
 import AuthUser from "../external/authUser";
-import {AlertMessageComponent} from "../components/alertMessageComponent";
-import {useState} from "react";
 
-export default function MainScreen(){
-    const [messageState, setMessageState] = useState(null);
-    const [messageStyleState, setMessageStyleState] = useState("info")
-
+export default function MainScreen({navigation}){
     return(
         <View style={{selfAlign: "center", marginHorizontal: "45%", marginVertical: "25%"}}>
-            <AlertMessageComponent
-                message={messageState}
-                chosenStyle={messageStyleState}
-                setMessageCallback={setMessageState}/>
             <Button title="Logout" onPress={()=>{
                 AuthUser.signOut().then(() => {}).catch((error) => {
                     console.log("Error at sign out: ", error.message);
-                    setMessageStyleState("danger");
-                    setMessageState("An unknown error occurred, please try again.");
                 })
             }}/>
+            <Button title="Go to ChatScreen" onPress={() => {navigation.push("Chat")}}/>
+            <Button title="Go to ContactListScreen" onPress={() => {navigation.push("Contact list")}}/>
         </View>
     )
 }
