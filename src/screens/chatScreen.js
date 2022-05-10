03e9ac369e95ed8736ccf7a1d86190ce0900a122
Katrin Kahur <kahur@kth.se>
2022-05-10 19:07:39 +0200

    Created a placeholder for ChatScreen.

---
+++ src/screens/chatScreen.js
@@ -0,0 +1,7 @@
+import {Text, View} from "react-native";
+
+export default function ChatScreen(){
+    return (
+        <View><Text style={{fontSize: 30, textAlign: "center"}}>Chat screen</Text></View>
+    )
+}
