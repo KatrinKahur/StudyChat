da082a43e7213b0e81916fb0b9f2bc14df672d98
Raminking01 <71043231+Raminking01@users.noreply.github.com>
2022-05-12 15:25:16 +0200

    Ramins commit

--- src/screens/chatScreen.js
+++ src/screens/chatScreen.js
@@ -1,7 +1,42 @@
-import {Text, View} from "react-native";
+import {Button, TextInput, View} from "react-native";
+import React from "react";
+import {getDatabase, push, ref} from "firebase/database";
+import moment from "moment";
+
+
+function addMessageToDatabase(to, from, message){
+    push(ref(getDatabase(), '/messages'), {
+        time: moment()
+            .utcOffset('+02:00')
+            .format('YYYY-MM-DD HH:mm:ss'),
+        to: to,
+        from: from,
+        message: message
+    });
+
+}
 
 export default function ChatScreen(){
-    return (
-        <View><Text style={{fontSize: 30, textAlign: "center"}}>Chat screen</Text></View>
+    const[userFrom, setUserFrom] = React.useState("Pelle")
+    const[userTo, setUserTo] = React.useState("Kalle")
+    const[currentMessage, setCurrentMessage] = React.useState("");
+    const[messageSentStatus, setMessageSentStatus] = React.useState(false);
+
+    React.useEffect(() => {
+        if(messageSentStatus){
+            addMessageToDatabase(userTo, userFrom, currentMessage)
+            setMessageSentStatus(false);
+            setCurrentMessage("");
+        }
+    }, [messageSentStatus])
+
+    return(
+        <View>
+            <TextInput
+                value={currentMessage}
+                onChangeText={(message) => setCurrentMessage(message)}
+                placeholder="Enter a message..."/>
+            <Button title="Send message" onPress={() => {setMessageSentStatus(true)}}/>
+        </View>
     )
 }
