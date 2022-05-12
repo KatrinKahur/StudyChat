ae0df4c8696b2c3fc0f43aa8401b63ec70f03db1
Katrin Kahur <kahur@kth.se>
2022-05-12 17:10:51 +0200

    Created some tests for AlertMessageComponent.

---
+++ __tests__/components/alertMessageComponent.test.js
@@ -0,0 +1,87 @@
+import {AlertMessageComponent} from "../../src/components/alertMessageComponent";
+import renderer from 'react-test-renderer';
+import {render, cleanup} from "@testing-library/react-native";
+import { toHaveStyle, toHaveTextContent } from "@testing-library/jest-native";
+import '@testing-library/jest-dom'
+
+beforeEach(() => {
+    cleanup();
+})
+
+describe('AlertMessageComponent', () => {
+    expect.extend({ toHaveStyle, toHaveTextContent });
+    it('should render correctly', () => {
+        let testMessage = "Random message";
+        const setTestMessage = (message) => {
+            testMessage = message;
+        }
+        let style = "danger"
+        const testProps = {
+            message: testMessage,
+            chosenStyle: style,
+            setMessageCallback: setTestMessage
+        }
+
+        const { toJSON } = renderer.create(
+            <AlertMessageComponent {...testProps} />
+        )
+        expect(toJSON()).toMatchSnapshot();
+    });
+
+    it('should render "info"-style if chosenStyle not specified', () => {
+        let testMessage = "Random message";
+        const setTestMessage = (message) => {
+            testMessage = message;
+        }
+        let style = ""
+        const testProps = {
+            message: testMessage,
+            chosenStyle: style,
+            setMessageCallback: setTestMessage
+        }
+        const { getByText, getByTestId } = render(<AlertMessageComponent
+            {...testProps} />);
+        expect(getByText(testMessage)).toHaveStyle({
+            color: "#31708f"
+        })
+        expect(getByTestId('message-container')).toHaveStyle({
+            backgroundColor: "#d9edf7",
+            borderColor: "#bce8f1"
+        });
+    });
+
+    it('should render an empty view if message property is null', () => {
+        let testMessage = null;
+        const setTestMessage = (message) => {
+            testMessage = message;
+        }
+        let style = ""
+        const testProps = {
+            message: testMessage,
+            chosenStyle: style,
+            setMessageCallback: setTestMessage
+        }
+        const { queryByTestId } = render(<AlertMessageComponent
+            {...testProps} />);
+        expect(queryByTestId('message-container')).toBeNull();
+    });
+
+    /*it('should become invisible when pressed on', async () => {
+        await act(async () => {
+            let testMessage = "Random message";
+            const setTestMessage = (message) => {
+                testMessage = message;
+            }
+            let style = ""
+            const testProps = {
+                message: testMessage,
+                chosenStyle: style,
+                setMessageCallback: setTestMessage
+            }
+            const { queryByTestId, getByText, findByText } = render(<AlertMessageComponent
+                {...testProps} />);
+            fireEvent.press(queryByTestId('message-container'));
+            expect(await queryByTestId('message-container')).toBeTruthy();
+        })
+    });*/
+})
