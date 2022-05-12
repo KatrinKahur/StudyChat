a71812e23873a6789d529f45054d0956e559f69c
vrin <vrin@kth.se>
2022-05-12 17:54:26 +0200

    added contact list screen

--- src/screens/contactListScreen.js
+++ src/screens/contactListScreen.js
@@ -1,28 +1,25 @@
-import { View, Button, Platform, Text } from "react-native";
+import { View, Button, Platform, Text, StatusBar, FlatList, StyleSheet, ScrollView } from "react-native";
 import AuthUser from "../external/authUser";
 import MainScreen from "./mainScreen";
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
 import { initializeAuth } from "firebase/auth";
-
 import { firebaseConfig } from "../config/firebaseConfig";
-
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getReactNativePersistence } from "firebase/auth/react-native";
-
 import React, { useState } from "react";
-
 import { getDatabase, ref, set, onValue, child, get } from "firebase/database";
 
 // Import Admin SDK
 export default function ContactListScreen({ navigation }) {
 
+
+
   const [users, setUsers] = React.useState([]);
 
-  const [newArray, setnewArray] = useState();
+  const [newArray, setnewArray] = useState([]);
 
   function writeUserData(userId, name, email, imageUrl) {
     const db = getDatabase();
@@ -56,16 +53,59 @@ export default function ContactListScreen({ navigation }) {
 
   GetAllDataOnce();
 
+
+  function renderUsers(){
+    {users.map((users) => (
+      <Text className="contactList" key={users.username}>{users.username}</Text>
+    ))}
+  }
+
+
   return (
     <View style={{ selfAlign: "center", margin: "30%" }}>
-      <Text>CONTACT LIST SCREN</Text>
-      <Button title="Main" onPress={() => navigation.navigate('MainScreen')} />
-      <Button title="save to Database" onPress={() => writeUserData(0, "0bdullah", "michael@gmail.com", "URL.RANDOM")} />
+      <Button title="save to Database" onPress={() => writeUserData(0, "POPbdullah", "michael@gmail.com", "URL.RANDOM")} />
       <Button title="getData" onPress={() => GetAllDataOnce()}></Button>
+      
+      <View style={styles.container}>
+
+
+      <ScrollView>
       {users.map((users) => (
-        <Text  key={users.username}>{users.username}</Text>
-      ))}
+
+      <View key={users.username}>
+      <Text style={styles.item} key={users.username}>{users.username}</Text>
+        </View>
+    ))}
+
+      </ScrollView>
+      </View>
+
+      
 
     </View>
   )
-}
+  }
+ 
+      
+      
+     
+
+  
+  
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 20,
+    paddingHorizontal: 10
+}, 
+  item:{
+    marginTop:24,
+    padding:60,
+    backgroundColor: 'pink',
+    fontSize: 24
+  }
+});
+
