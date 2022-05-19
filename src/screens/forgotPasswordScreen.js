81f59ea5fb4d8e5cc355cf413aa77d501a33a2c9
Abdullah Trabulsiah <abdtra@kth.se>
2022-05-19 11:17:02 +0200

    Ändrade tillbaka till det gamla så att ingen ska klaga på att man tar inspiration från andra

--- src/screens/forgotPasswordScreen.js
+++ src/screens/forgotPasswordScreen.js
@@ -9,62 +9,60 @@ import {
   Platform,
 } from "react-native";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
-import { EmailInput } from "../components/emailInput";
-import { AlertMessageComponent } from "../components/alertMessageComponent";
-import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
-export default function ForgotPassword({ navigation }) {
-  const [authStatus, setAuthStatus] = React.useState(0);
-  const [email, setEmail] = React.useState("");
-  const [errorMessageState, setErrorMessageState] = useState(null);
+export default class ForgotPassword extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+    };
+  }
 
-  React.useEffect(
-    function () {
-      if (authStatus > 0)
-        sendPasswordResetEmail(getAuth(), email)
-          .then(() => {
-            console.log("Password reset email sent successfully");
-          })
-          .catch((error) => {
-            if (error.message === "Invalid email") {
-              setErrorMessageState(error.message);
-            } else {
-              console.log(error.message);
-              setErrorMessageState(
-                "An unknown error occurred, please try again."
-              );
-            }
-          });
-    },
-    [authStatus]
-  );
+  updateInputVal = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.forgotPasswordContainer}>
+  handlePasswordReset = () => {
+    sendPasswordResetEmail(getAuth(), this.state.email).then(() => {
+      console.log("Password reset email sent successfully");
+    });
+    this.props.navigation.navigate("Sign-in");
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
         <Text style={styles.welcomeText}>Reset your password</Text>
-        <AlertMessageComponent
-          message={errorMessageState}
-          chosenStyle="danger"
-          setMessageCallback={setErrorMessageState}
-        />
-        <View style={styles.forgotPasswordInputContainer}>
-          <EmailInput onChangeText={(val) => setEmail(val)} value={email} />
+        <View style={styles.ForgotPassword}>
+          <TextInput
+            style={styles.textinput}
+            placeholder="Enter a registred email please!"
+            value={this.state.email}
+            onChangeText={(val) => this.updateInputVal(val, "email")}
+          />
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {
-              if (email === "")
-                setErrorMessageState("Please fill in the credentials");
-              else setAuthStatus(authStatus + 1);
-            }}
+            onPress={(email) => this.handlePasswordReset()}
           >
             <Text style={styles.buttonTitle}>Send email</Text>
           </TouchableOpacity>
         </View>
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
