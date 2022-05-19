5f697f600b235d423bf4a1d7c8897037e2480869
Abdullah Trabulsiah <abdtra@kth.se>
2022-05-19 02:07:28 +0200

    Fixed forgot password screen and modified sign up screen
    Forgot password screen is done and tested.
    Sign up screen has been modified so that displayName (username) should be available to fetch from firebase when needed.

--- src/screens/forgotPasswordScreen.js
+++ src/screens/forgotPasswordScreen.js
@@ -1,86 +1,110 @@
-import React, { Component, Fragment } from 'react';
-import { Text, SafeAreaView, View, StyleSheet } from 'react-native';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import FormInput from './src/components/FormInput';
-import Button from './src/components/Button';
-import ErrorMessage from '../components/ErrorMessage';
-import { withFirebaseHOC } from '../config/firebaseConfig';
+import React, { useState } from 'react';
+import { Text, SafeAreaView, View, StyleSheet, TextInput, TouchableOpacity, Platform } from 'react-native';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { EmailInput } from '../components/emailInput';
+import {AlertMessageComponent} from "../components/alertMessageComponent";
+import {StatusBar} from "expo-status-bar";
 
-const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .label('Email')
-      .email('Enter a valid email')
-      .required('Please enter a registered email')
-  });
 
-class ForgotPassword extends Component {
-  render() {
+
+export default function ForgotPassword ({navigation}){
+  
+  const[authStatus, setAuthStatus] = React.useState(0);
+  const[email, setEmail] = React.useState("");
+  const [errorMessageState, setErrorMessageState] = useState(null);
+
+  React.useEffect(function (){
+    if(authStatus > 0)
+    sendPasswordResetEmail(getAuth(), email).then(() => {
+      console.log('Password reset email sent successfully')
+    }).catch((error) => {
+            if(error.message === "Invalid email") {
+                setErrorMessageState(error.message);
+            }
+            else {
+                console.log(error.message);
+                setErrorMessageState("An unknown error occurred, please try again.");
+            }
+        })
+}, [authStatus]);
+
     return (
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.text}>Forgot Password?</Text>
-          <Formik
-            initialValues={{ email: '' }}
-            onSubmit={(values, actions) => {
-              this.handlePasswordReset(values, actions)
+        <View style={styles.container}>
+          <View style={styles.forgotPasswordContainer}>
+          <Text style={styles.welcomeText}>
+             Reset your password
+          </Text>
+          <AlertMessageComponent
+                    message={errorMessageState}
+                    chosenStyle="danger"
+                    setMessageCallback={setErrorMessageState}/>
+          <View style={styles.forgotPasswordInputContainer}>
+            <EmailInput 
+              onChangeText={(val) => setEmail(val)}
+              value={email}
+            />
+            <TouchableOpacity 
+              style={styles.button}
+              onPress={()=>{
+                if(email === "")
+                    setErrorMessageState("Please fill in the credentials");
+                else
+                   setAuthStatus(authStatus + 1);
             }}
-            validationSchema={validationSchema}>
-            {({
-              handleChange,
-              values,
-              handleSubmit,
-              errors,
-              isValid,
-              touched,
-              handleBlur,
-              isSubmitting
-            }) => (
-              <Fragment>
-                <FormInput
-                  name='email'
-                  value={values.email}
-                  onChangeText={handleChange('email')}
-                  placeholder='Enter email'
-                  autoCapitalize='none'
-                  iconName='ios-mail'
-                  iconColor='#2C384A'
-                  onBlur={handleBlur('email')}
-                />
-                <ErrorMessage errorValue={touched.email && errors.email} />
-                <View style={styles.buttonContainer}>
-                  <Button
-                    buttonType='outline'
-                    onPress={handleSubmit}
-                    title='Send Email'
-                    buttonColor='#039BE5'
-                    disabled={!isValid || isSubmitting}
-                  />
-                </View>
-                <ErrorMessage errorValue={errors.general} />
-              </Fragment>
-            )}
-          </Formik>
-        </SafeAreaView>
+            >
+              <Text style={styles.buttonTitle}>Send email</Text>
+            </TouchableOpacity>
+          </View>
+          </View>
+          <StatusBar style="auto"/>
+        </View>
       )
-  }
 }
 
 const styles = StyleSheet.create({
     container: {
+      display: "flex",
+      width: "100%",
       flex: 1,
-      backgroundColor: '#fff',
-      marginTop: 150
+      flexDirection: "row",
+      backgroundColor: "white"
     },
-    text: {
-      color: '#333',
-      fontSize: 24,
-      marginLeft: 25
+    forgotPasswordInputContainer: {
+      width: Platform.OS === 'web'? "15%":"55%",
+      minWidth: Platform.OS === 'web'? 340 : 0,
+  },
+    forgotPasswordContainer: {
+      justifyContent: "center",
+        alignItems: "center",
+        flex: 1
     },
     buttonContainer: {
       margin: 25
-    }
+    },
+    button: {
+      backgroundColor: `#1e90ff`,
+      paddingVertical: 17,
+      marginTop: 10,
+      marginBottom: 15,
+      alignItems: "center",
+      borderRadius: 5
+  },
+    textinput: {
+      alignSelf: 'stretch',
+      height: 40,
+      marginBottom: 30,
+      color: '#000000',
+      borderBottomColor: '#000000',
+      borderBottomWidth: 1,
+  },
+    buttonTitle: {
+    fontSize:  Platform.OS === 'web'? 20 : 15,
+    color: `#ffffff`
+  },
+  welcomeText: {
+    fontSize: Platform.OS === 'web' ? 65 : 40,
+    marginBottom: Platform.OS === 'web' ? 50 : 35,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
   });
-
-
-
-export default withFirebaseHOC(ForgotPassword);
