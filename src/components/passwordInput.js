2fb3938c69663975244fac73a7d2ccda5feec7ce
Katrin Kahur <kahur@kth.se>
2022-05-23 10:27:41 +0200

    Added Enter key event for better usability.

--- src/components/passwordInput.js
+++ src/components/passwordInput.js
@@ -2,14 +2,16 @@ import {TextInput, View, StyleSheet, Platform} from "react-native";
 import {FontAwesome5} from "@expo/vector-icons";
 import React from "react";
 
-export const PasswordInput = ({onChangeText, value}) => {
+export const PasswordInput = ({onChangeText, onSubmitEditing, value}) => {
     return(
         <View style={styles.container}>
             <FontAwesome5 name="lock" size={24} color="black" />
-            <TextInput testID="passwordInput"
-                       style={styles.input} value={value}
-                       secureTextEntry={true}
-                       onChangeText = {onChangeText} placeholder="Password.."/>
+            <TextInput
+                testID="passwordInput"
+                style={styles.input} value={value}
+                secureTextEntry={true}
+                onSubmitEditing={onSubmitEditing}
+                onChangeText = {onChangeText} placeholder="Password.."/>
         </View>
     )
 }
